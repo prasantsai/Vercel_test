18df136e72cfd6b4842a14e03edfcd31488dc8f5
prasantsai <gandreti.prasantsai@hcl.com>
2023-12-13 21:01:20 +0530

    message

--- api/index.py
+++ api/index.py
@@ -18,21 +18,19 @@ def home():
     return 'Hello, World!'
 
 @app.route('/conversation',methods=['POST'])
-def conversation_func():
-    if request.method == 'POST':
-        user_input = request.form['user_input']
-        template = """ You are a conversational Bot. Respond politely in just one to two short sentences to human input.
-        Current conversation:
-        {history}
-        Human: {input}
-        Bot:
-        """
-        prompt_template = PromptTemplate(input_variables=["input","history"], template=template)
+def conversation_func(user_input):
+    template = """ You are a conversational Bot. Respond politely in just one to two short sentences to human input.
+    Current conversation:
+    {history}
+    Human: {input}
+    Bot:
+    """
+    prompt_template = PromptTemplate(input_variables=["input","history"], template=template)
 
-        conversation = ConversationChain(
-            llm=llm,
-            prompt=prompt_template,
-            verbose=False,
-            memory=memory
-        )
+    conversation = ConversationChain(
+        llm=llm,
+        prompt=prompt_template,
+        verbose=False,
+        memory=memory
+    )
     return jsonify(conversation.predict(input=user_input))
